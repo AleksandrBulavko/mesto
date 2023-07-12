9c4d5039a33c9a82520d3311aed6db624b805ee4
Aleksandr Bulavko <Sanbox_99@mail.ru>
2023-07-12 21:26:50 +0500

    Седьмая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popuptypeEditProfile = document.querySelector('.popup_type_edit-profile');
 const popupCloseButtonProfile = document.querySelector('.popup__close-button_place_edit-profile');
-const inputName = document.querySelector('.popup__input_edit-profile_name');
-const inputAbout = document.querySelector('.popup__input_edit-profile_about');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputAbout = document.querySelector('.popup__input_type_about');
 const formEditProfile = document.querySelector('.popup__form_place_edit-profile');
 const formName = document.querySelector('.profile__name');
 const formAbout = document.querySelector('.profile__about-me');
 
 
 function openPopup(popup) {
-    popup.classList.add("popup_opened")
+    popup.classList.add('popup_opened')
 }
 function closePopup(popup) {
-    popup.classList.remove("popup_opened")
+    popup.classList.remove('popup_opened')
 }
 
 function handleFormSubmit(evt) {
@@ -22,13 +22,13 @@ function handleFormSubmit(evt) {
     closePopup(popuptypeEditProfile)
 }
 
-profileEditButton.addEventListener("click", () => {
+profileEditButton.addEventListener('click', () => {
     inputName.value = formName.textContent
     inputAbout.value = formAbout.textContent
     openPopup(popuptypeEditProfile)
 })
-popupCloseButtonProfile.addEventListener("click", () => closePopup(popuptypeEditProfile))
-formEditProfile.addEventListener("submit", function (event) {
+popupCloseButtonProfile.addEventListener('click', () => closePopup(popuptypeEditProfile))
+formEditProfile.addEventListener('submit', function (event) {
     handleFormSubmit(event)
 })
 
